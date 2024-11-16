75e52a164538b4dc2c0a8b23721b176b5cca2cc2
KeerthiKanoor <kanoorkeeerhi59@gmail.com>
2024-11-16 12:35:35 +0530

    Create Bridge.java

---
+++ visaa_prep/Bridge.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        int z=sc.nextInt();
+        int maxmango=(z-y)/x;
+       
+        System.out.println(maxmango);
+
+    }
+}
