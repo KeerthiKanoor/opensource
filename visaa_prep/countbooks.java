8fe628a36238a3376f159bb8ef23fc5e3943f6e0
KeerthiKanoor <kanoorkeeerhi59@gmail.com>
2024-11-16 20:14:08 +0530

    Create countbooks.java

---
+++ visaa_prep/countbooks.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int N=sc.nextInt();
+        int notebooks=N*10;  //N=100
+        System.out.println(notebooks);
+        
+    }
+}
