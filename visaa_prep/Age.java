e6d74ec55531bb605dd67c91ccc461f9aa913b26
KeerthiKanoor <kanoorkeeerhi59@gmail.com>
2024-11-16 20:26:50 +0530

    Create Age.java

---
+++ visaa_prep/Age.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int X=sc.nextInt();
+        int age=X-15;
+        System.out.println(age);
+        
+ 
+    }
+}
