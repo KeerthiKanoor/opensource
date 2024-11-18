af9c16a3497b98af273450dcea2f0f712c0e7a6b
KeerthiKanoor <kanoorkeeerhi59@gmail.com>
2024-11-18 11:29:25 +0530

    Create testScore.java

---
+++ visaa_prep/testScore.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int N=sc.nextInt();
+        int X=sc.nextInt();
+        int Y=sc.nextInt();
+        if (Y % X == 0 && Y / X <= N)
+        
+        
+        {
+            System.out.println("YES");
+            
+        }
+        else{
+            System.out.println("NO");
+        }
+        
+    }
+}
